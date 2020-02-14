cf628deb6205d7ac805b380dd313d63727167aff
Sébastien ALVAREZ <sebastien.alvarez@yahoo.fr>
2020-02-14 01:03:32 +0100

    Version initiale - 1ère asset Unity - V2 rocket

---
+++ V2_ASSET/Assets/V2 Rocket Asset/Assets/Script/CameraTracking.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour {
+    /*  Camera Tracking script for V2 Rocket Asset
+    *  Author : Sebastien ALVAREZ
+    *  Version : 1.0 
+    *  Date : 6 may 2018
+    *  Purpose : Hide canvas and camera tracking of V2 rockets
+    *  Contact : sebastien.alvarez@yahoo.fr
+    */
+
+    public Canvas canvas; // Canvas object to hide the canvas with asset description and command instruction
+    public GameObject V2; // V2 Gameobject for tracking purpose
+    private bool isLaunchAsked = false; // Variable for user launch command detection
+    public float rotationSpeed = 0.2f; // Variable to parameter the rotation speed when waiting for launch
+    private Vector3 initialPosition; // variable to save initial position of the camera
+
+
+    private void Start()
+    {
+        initialPosition = gameObject.transform.position;
+    }
+
+    void Update () {
+        // Rotate the camera around the V2 rockets when waiting for space bar pressing
+        if (!isLaunchAsked)
+        {
+            gameObject.transform.position = Quaternion.AngleAxis(rotationSpeed, Vector3.up) * gameObject.transform.position;
+        }
+
+        // Hide the canvas when the user press Space Bar to start asset demonstration
+        if (Input.GetKeyDown(KeyCode.Space) && !isLaunchAsked)
+        {
+            // ===> Uncomment the following code line to get the initial camera position at launch
+            //gameObject.transform.position = initialPosition;
+            isLaunchAsked = true;
+            canvas.enabled = false;
+        }
+
+        // Camera tracking of V2 rockets
+        gameObject.transform.rotation = Quaternion.LookRotation(V2.transform.position - gameObject.transform.position);
+    }
+}
---
+++ V2_ASSET/Assets/V2 Rocket Asset/Assets/Script/V2Launch.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V2Launch : MonoBehaviour {
+    /*  V2 Launch script for V2 Rocket Asset
+     *  Author : Sebastien ALVAREZ
+     *  Version : 1.0 
+     *  Date : 6 may 2018
+     *  Purpose : Play 2 free sounds (countdown and rocket launch), activate rocket engine particle system and add velocity to the V2 for demonstration purpose
+     *  Contact : sebastien.alvarez@yahoo.fr
+    */
+
+    public AudioClip countDownSound; // Variable to get countdown sound (sound file to get in Unity Editor)
+    public AudioClip rocketLaunchSound; // Variable to get rocket launch sound (sound file to get in Unity Editor)
+    public GameObject exaust; // GameObject to activate rocket engine exaust
+    public float mass = 13000f; // Variable for rocket mass (25000kg is real value for the V2)
+    public float drag = 0.15f; // Variable for drag coefficient
+    public float dryMass = 4390f; // Variable for rocket dry mass (4390kg is real value for the V2)
+    public float thrust = 25000f; // Variable for rocket engine thrust (25000kg is real value for the V2, in script it is multiplied by 9.8m/s²)
+    public float consumption = 125f; // Variable for fuel consumption (125kg/s is real value for the V2)
+    private bool isLaunchAsked = false; // Variable for user launch command detection
+    private bool isLaunchStart = false; // Variable for launch detection after the countdown 
+    private AudioSource audioSourceCountDown; // AudioSource object to play countdown sound
+    private AudioSource audioSourceRocketlaunch; // AudioSource object to play rocket launch sound
+    private Rigidbody rocket; // Rigibody object to apply physics on the V2 rocket
+
+	void Start () {
+        audioSourceCountDown = GetComponents<AudioSource>()[0];
+        audioSourceRocketlaunch = GetComponents<AudioSource>()[1];
+        rocket = GetComponent<Rigidbody>();
+        rocket.mass = mass;
+        rocket.drag = drag;
+        exaust.SetActive(false);
+	}
+	
+	void Update () {
+        // Detection of launch command from the user
+        if(Input.GetKeyDown(KeyCode.Space) && !isLaunchAsked)
+        {
+            isLaunchAsked = true;
+            // Start 10s countdown
+            StartCoroutine(StartCountDown());
+        }		
+	}
+
+    private void FixedUpdate()
+    {
+        if (isLaunchStart)
+        {
+            if(rocket.mass >= dryMass)
+            {
+                // Physics application on the rocket
+                rocket.AddForce(9.8f * thrust * gameObject.transform.forward, ForceMode.Force);
+                rocket.mass -= consumption * Time.deltaTime;
+            }
+            else
+            {
+                exaust.SetActive(false);
+            }
+        }    
+    }
+
+    // Countdown temporisation method
+    IEnumerator StartCountDown()
+    {
+        audioSourceCountDown.volume = 0.1f;
+        audioSourceCountDown.PlayOneShot(countDownSound);
+        yield return new WaitForSeconds(10.0f);
+        StartCoroutine(StartRocketLaunch());
+    }
+
+    IEnumerator StartRocketLaunch()
+    {
+        audioSourceRocketlaunch.volume = 0.8f;
+        audioSourceRocketlaunch.PlayOneShot(rocketLaunchSound);
+        yield return new WaitForSeconds(7f);
+        exaust.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        isLaunchStart = true;
+    }
+}
